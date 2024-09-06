f67ff6ebfd9b952a7594d500634de461dfcb4828
bharath99 <60287182+BharathGangatharan@users.noreply.github.com>
2024-09-06 07:39:01 +0530

    mobile css updates

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React,{useState} from "react";
 import style from "./Header.module.css";
 import logo from "../../assets/icons/logo.png";
 import menu from "../../assets/icons/MenuIcon.png";
+import BulletPoint from "../../components/BulletPoint/BulletPoint";
 
 const Header = () =>{
 
+    const [showNavigation,setShowNaviagtion] = useState(false)
+
     const scrollToSection = (sectionId) => {
         document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
-    }; 
+        setShowNaviagtion(false);
+    };
 
     return(
         <div className={style.header_container}>
@@ -15,10 +19,35 @@ const Header = () =>{
                 <img src={logo} alt="Logo"/>
             </div>
             <div className={style.menu_container}>
-                <div className={style.menu_text}>MENU</div>
-                <div className={style.menu_icon}>
+                <div className={style.menu_icon} onClick={()=>{setShowNaviagtion(true)}}>
                     <img src={menu} alt="Menu"/>
                 </div>
+            </div>
+            <div className={`${showNavigation ? style.mobile_menu_bar_show : style.mobile_menu_bar_hide} ${style.mobile_menu_bar}`}>
+                    <div className={style.menu_items}>
+                        <div onClick={() => scrollToSection('about')} className={`${style.about_text} ${style.menu_texts}`}>
+                            ABOUT
+                            <span><BulletPoint/></span>
+                        </div>
+                        <div className={style.about_desc}>WHO WE ARE</div>
+                    </div>
+                    <div className={style.menu_items}>
+                        <div onClick={() => scrollToSection('service')} className={`${style.service_text} ${style.menu_texts}`}>
+                            SERVICES
+                            <span><BulletPoint/></span>
+                        </div>
+                        <div className={style.service_desc}>WHAT WE DO</div>
+                    </div>
+                    <div className={style.menu_items}>
+                        <div onClick={() => scrollToSection('contact')} className={`${style.contact_text} ${style.menu_texts}`}>
+                            CONTACT
+                            <span><BulletPoint/></span>
+                        </div>
+                        <div className={style.contact_desc}>GET IN TOUCH</div>
+                    </div>
+            </div>
+            <div className={showNavigation ? style.close_show : style.close_hide} onClick={()=>{setShowNaviagtion(false)}}>
+                X
             </div>
         </div>
     )
